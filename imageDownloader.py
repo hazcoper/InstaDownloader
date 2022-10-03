92b069d729afc7ad1715fd0afb1f76ace7a861f6
hazcoper <zemariamateus@gmail.com>
2022-10-03 07:23:31 +0100

    some comements and created function that will be used to log in

--- imageDownloader.py
+++ imageDownloader.py
@@ -8,9 +8,14 @@ def downloadImage(SHORTCODE, folderName=None):
 
     os.system(f"instaloader -l hazprinter --dirname-pattern output -- -{SHORTCODE}")
 
+def downloadStory(code, folderName=None):
+    pass
 
 
 def cleanCache():
+    """
+    Cleans the files that where downloaded by instaloader
+    """
     folderList = [x for x in os.listdir() if x == "output"]
     
     for folder in folderList:
